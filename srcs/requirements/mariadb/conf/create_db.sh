b6266528f85aa5ad47b3d6453a26f187b0fc6ce4
0xakhii <ojamal@student.1337.ma>
2024-04-07 22:43:08 +0000

    Added Dockerfile to set up a Debian-based MariaDB server with custom configuration

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,9 +1,8 @@
-service mariadb start;
-
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';"
+#!/bin/bash
+service mariadb start
+mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mariadb -e "CREATE user IF NOT EXISTS $DB_USER IDENTIFIED BY '$DB_PASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER;"
 mariadb -e "FLUSH PRIVILEGES;"
 service mariadb stop
-mariadb
+mariadbd
