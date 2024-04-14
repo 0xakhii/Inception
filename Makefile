932adcf42ad2da978d2af426a2bd3876b67251ea
0xakhii <ojamal@student.1337.ma>
2024-04-14 22:48:40 +0100

    Update Makefile and environment variables

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ all :
 	sudo docker-compose -f srcs/docker-compose.yml build && sudo docker-compose -f srcs/docker-compose.yml up
 down : 
 	sudo docker-compose -f srcs/docker-compose.yml down
+
+up :
+	sudo docker-compose -f srcs/docker-compose.yml up
--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wp-cli.phar core download --allow-root
-wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root
-wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_PASSWD --admin_email=$ADMIN_EMAIL --allow-root
+wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=mariadb --allow-root
+wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_ROOT_PASSWD --admin_email=$ADMIN_EMAIL --allow-root
 wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --role=subscriber --user_pass=$USER1_PASS --allow-root
 php-fpm8.2 -F -R
