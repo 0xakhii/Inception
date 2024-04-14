5d39e0c200e350dca79086f20a13e07e1cacaaa7
0xakhii <ojamal@student.1337.ma>
2024-04-14 21:37:01 +0100

    Update database and user credentials, bind data directories, and modify server name

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-service mariadb start
-mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mariadb -e "CREATE user IF NOT EXISTS $DB_USER IDENTIFIED BY '$DB_PASS';"
-mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER;"
-mariadb -e "FLUSH PRIVILEGES;"
-service mariadb stop
-mariadbd
+service mariadb start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';"
+mysql -e "FLUSH PRIVILEGES;"
+mysqladmin -u root -p$DB_ROOT_PASSWD shutdown
+exec mysqld_safe
