f00c2724a10dd4768f9de2b991acc550cee190be
0xakhii <ojamal@student.1337.ma>
2024-04-18 01:58:52 +0100

    Update MariaDB Dockerfile and create_db.sh script

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -4,6 +4,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$DB_ROOT_PASSWD shutdown
+mysqladmin -u root $DB_ROOT_PASSWD
+mysql -u root -p$DB_ROOT_PASSWD -e "FLUSH PRIVILEGES;"
+mysql -u root -p$DB_ROOT_PASSWD shutdown
 exec mysqld_safe
