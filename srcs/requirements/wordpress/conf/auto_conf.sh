b3e30c58b10319bf4ad9f845c741dc5e4f1c465d
0xakhii <ojamal@student.1337.ma>
2024-04-19 22:26:37 +0100

    Update environment variables and database configuration

--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10;
 wp-cli.phar core download --allow-root
 wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=mariadb --allow-root
 wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_ROOT_PASSWD --admin_email=$ADMIN_MAIL --allow-root
