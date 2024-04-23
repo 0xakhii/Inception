f41a5749fa6cf9129e4d6db1a4cf54875b15816f
0xakhii <ojamal@student.1337.ma>
2024-04-23 19:14:42 +0100

    Update network name in docker-compose.yml and set ownership for wp-content directory in auto_conf.sh

--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -5,6 +5,7 @@ wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSW
 wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_ROOT_PASSWD --admin_email=$ADMIN_MAIL --allow-root
 wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --role=subscriber --user_pass=$USER1_PASS --allow-root
 chmod -R 777 /var/www/html/wp-content/
+chown -R www-data:www-data /var/www/html/wp-content/
 chmod -R 777 /var/www/html/wp-content/plugins
 chmod -R 777 /var/www/html/wp-content/themes
 chmod -R 777 /var/www/html/wp-content/uploads
