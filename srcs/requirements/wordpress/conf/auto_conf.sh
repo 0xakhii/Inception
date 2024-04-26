fc0903fc757bc1c631ee89bc36b043fc85bd6804
0xakhii <ojamal@1337.student.ma>
2024-04-26 23:12:37 +0100

    Refactor Dockerfiles for better readability and maintenance

--- srcs/requirements/wordpress/conf/auto_conf.sh
+++ srcs/requirements/wordpress/conf/auto_conf.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-wp-cli.phar core download --allow-root && \
-wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=mariadb --allow-root && \
-wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_ROOT_PASSWD --admin_email=$ADMIN_MAIL --allow-root && \
-wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --role=subscriber --user_pass=$USER1_PASS --allow-root && \
-chmod -R 777 /var/www/html/wp-content/plugins && \
-chmod -R 777 /var/www/html/wp-content/themes && \
-chmod -R 777 /var/www/html/wp-content/uploads && \
-wp-cli.phar plugin install redis-cache --activate --allow-root && \
-chown -R www-data:www-data /var/www/html/ && \
-wp-cli.phar config set WP_CACHE true --raw --type=constant --allow-root && \
-wp-cli.phar	config set WP_REDIS_HOST redis --allow-root && \
-wp-cli.phar redis enable --allow-root && \
-mkdir -p /run/php/ && \
+sleep 2
+wp-cli.phar core download --allow-root
+wp-cli.phar config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=mariadb --allow-root
+wp-cli.phar core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$DB_ROOT --admin_password=$DB_ROOT_PASSWD --admin_email=$ADMIN_MAIL --allow-root
+wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --role=subscriber --user_pass=$USER1_PASS --allow-root
+chmod -R 777 /var/www/html/wp-content/plugins
+chmod -R 777 /var/www/html/wp-content/themes
+chmod -R 777 /var/www/html/wp-content/uploads
+wp-cli.phar plugin install redis-cache --activate --allow-root
+chown -R www-data:www-data /var/www/html/
+wp-cli.phar config set WP_CACHE true --raw --type=constant --allow-root
+wp-cli.phar	config set WP_REDIS_HOST redis --allow-root
+wp-cli.phar redis enable --allow-root
+mkdir -p /run/php/
 php-fpm7.4 -F -R 
